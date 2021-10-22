de89575eb4fd0031331bd332e79213dd43994870
Nello-Moreira <g.moreira1@outlook.com>
2021-10-22 17:42:18 -0300

    implemented: login return user name too

--- src/routes/login.js
+++ src/routes/login.js
@@ -46,9 +46,11 @@ async function postLogin(request, response) {
 
 		const token = result.rows[0].token;
 
-		return response
-			.status(200)
-			.send({ userId: user.rows[0].user_id, token });
+		return response.status(200).send({
+			userId: user.rows[0].user_id,
+			name: user.rows[0].name,
+			token,
+		});
 	} catch (error) {
 		return internalErrorResponse(response, error);
 	}
